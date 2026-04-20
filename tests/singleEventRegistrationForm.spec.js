c86389c0177c7cb3efd243ec4378973d17a7d797
Dylan Burnham <dburnham1212@gmail.com>
2026-04-20 11:37:18 -0400

    Adding more tests

--- tests/singleEventRegistrationForm.spec.js
+++ tests/singleEventRegistrationForm.spec.js
@@ -1,27 +1,44 @@
 import { test, expect } from "@playwright/test";
-import { loginAsUser } from "./utils/helper";
+import { createTestEvent, loginAsAdmin, loginAsUser } from "./utils/helper";
 
-test.beforeEach(async ({ page }) => {
-    await loginAsUser(page);
-    await page.goto("/events");
-});
+// test.beforeEach(async ({ page }) => {
+//     await loginAsUser(page);
+//     await page.goto("/events");
+// });
 
 test('Testing if clicking "Add To My Calendar" button adds the event successfully', async ({
     page,
 }) => {
+    // Login as admin and create test event
+    await loginAsAdmin(page);
+    const testName = await createTestEvent(page);
+
+    // Login as user so we can register
+    await loginAsUser(page);
+
+    // Check if registration works
+    await page.goto("/events");
     await page
-        .getByRole("link", { name: "Music May 14, 2026 Kingston" })
+        .getByRole('link', { name: `Test April 30, 2026 ${testName} Test` })
         .click();
     await page.getByRole("link", { name: "Register For This Event" }).click();
     await page.getByRole("button", { name: "Add To My Calendar" }).click();
     await expect(page.locator("body")).toContainText(
         "Registration created successfully.",
     );
+
+    // Log back in as admin and delete the event
+    await loginAsAdmin(page);
+    await page.getByRole('row', { name: `${testName} April 30,` }).getByRole('button').click();
+    await expect(page.locator('section')).toContainText('Event deleted successfully.');
+
 });
 
 test('Testing if clicking "Back To Event" button returns to the event successfully', async ({
     page,
 }) => {
+    await loginAsUser(page);
+    await page.goto("/events");
     await page
         .getByRole("link", { name: "Music May 14, 2026 Kingston" })
         .click();
@@ -33,29 +50,90 @@ test('Testing if clicking "Back To Event" button returns to the event successful
 test('Testing if clicking "Update My Seats" and updating seat count updates the registration', async ({
     page,
 }) => {
+    // Login as admin and create test event
+    await loginAsAdmin(page);
+    const testName = await createTestEvent(page);
+
+    // Login as user so we can register
+    await loginAsUser(page);
+
+    // Check register works with specified seat count
+    await page.goto("/events");
     await page
-        .getByRole("link", { name: "Music May 14, 2026 Kingston" })
+        .getByRole('link', { name: `Test April 30, 2026 ${testName} Test` })
         .click();
-    await page.getByRole("link", { name: "Update My Seats" }).click();
+    await page.getByRole("link", { name: "Register For This Event" }).click();
     await page.getByRole("spinbutton", { name: "Seat Count" }).click();
     await page.getByRole("spinbutton", { name: "Seat Count" }).fill("4");
-    await page.getByRole("button", { name: "Save Seat Changes" }).click();
+    await page.getByRole("button", { name: "Add To My Calendar" }).click();
     await expect(page.locator("body")).toContainText(
-        "Registration updated successfully.",
+        "Registration created successfully.",
     );
+
+    // Log back in as admin and delete the event
+    await loginAsAdmin(page);
+    await page.getByRole('row', { name: `${testName} April 30,` }).getByRole('button').click();
+    await expect(page.locator('section')).toContainText('Event deleted successfully.');
 });
 
 test("Testing if you can update seats to a negative value", async ({
     page,
 }) => {
+    // Login as admin and create test event
+    await loginAsAdmin(page);
+    const testName = await createTestEvent(page);
+
+    // Login as user so we can register
+    await loginAsUser(page);
+
+    // Check register works with negative seat count
+    await page.goto("/events");
     await page
-        .getByRole("link", { name: "Music May 14, 2026 Kingston" })
+        .getByRole('link', { name: `Test April 30, 2026 ${testName} Test` })
         .click();
     await page.getByRole("link", { name: "Register For This Event" }).click();
-    await page.getByRole("button", { name: "Add To My Calendar" }).click();
-    await page.getByRole("link", { name: "Edit Seats" }).click();
     await page.getByRole("spinbutton", { name: "Seat Count" }).click();
     await page.getByRole("spinbutton", { name: "Seat Count" }).fill("-1");
-    await page.getByRole("spinbutton", { name: "Seat Count" }).press("Enter");
-    await page.getByRole("button", { name: "Save Seat Changes" }).click();
+    await page.getByRole("button", { name: "Add To My Calendar" }).click();
+
+    const isValid = await page
+        .getByRole("spinbutton", { name: "Seat Count" })
+        .evaluate((el) => el.checkValidity());
+    expect(isValid).toBe(false);
+
+    // Log back in as admin and delete the event
+    await loginAsAdmin(page);
+    await page.getByRole('row', { name: `${testName} April 30,` }).getByRole('button').click();
+    await expect(page.locator('section')).toContainText('Event deleted successfully.');
+});
+
+test("Testing if you can update seats to an amount that exceeds max seat amount", async ({
+    page,
+}) => {
+    // Login as admin and create test event
+    await loginAsAdmin(page);
+    const testName = await createTestEvent(page);
+
+    // Login as user so we can register
+    await loginAsUser(page);
+
+    // Check register works with negative seat count
+    await page.goto("/events");
+    await page
+        .getByRole('link', { name: `Test April 30, 2026 ${testName} Test` })
+        .click();
+    await page.getByRole("link", { name: "Register For This Event" }).click();
+    await page.getByRole("spinbutton", { name: "Seat Count" }).click();
+    await page.getByRole("spinbutton", { name: "Seat Count" }).fill("11");
+    await page.getByRole("button", { name: "Add To My Calendar" }).click();
+
+    const isValid = await page
+        .getByRole("spinbutton", { name: "Seat Count" })
+        .evaluate((el) => el.checkValidity());
+    expect(isValid).toBe(false);
+
+    // Log back in as admin and delete the event
+    await loginAsAdmin(page);
+    await page.getByRole('row', { name: `${testName} April 30,` }).getByRole('button').click();
+    await expect(page.locator('section')).toContainText('Event deleted successfully.');
 });
--- tests/utils/helper.js
+++ tests/utils/helper.js
@@ -22,7 +22,33 @@ const loginAsUser = async (page) => {
     await expect(page.locator('body')).toContainText('You are now logged in.');
 }
 
+const createTestEvent = async (page) => {
+  const newEventName = `Test ${Date.now()}`
+  await page.goto('/admin/events/new');
+  await page.getByRole('link', { name: 'Add Event' }).click();
+  await page.getByRole('textbox', { name: 'Event Title' }).click();
+  await page.getByRole('textbox', { name: 'Event Title' }).fill(newEventName);
+  await page.getByRole('textbox', { name: 'Event Date' }).fill('2026-04-30');
+  await page.getByRole('textbox', { name: 'Location' }).click();
+  await page.getByRole('textbox', { name: 'Location' }).fill('Test');
+  await page.getByRole('textbox', { name: 'Category' }).click();
+  await page.getByRole('textbox', { name: 'Category' }).fill('Test');
+  await page.getByRole('textbox', { name: 'Image URL' }).click();
+  await page.getByRole('textbox', { name: 'Image URL' }).click();
+  await page.getByRole('textbox', { name: 'Image URL' }).click();
+  await page.getByRole('textbox', { name: 'Image URL' }).fill('https://jessup.edu/wp-content/uploads/2023/12/Programming-in-Computer-Science-NEW.webp');
+  await page.getByRole('textbox', { name: 'Description' }).click();
+  await page.getByRole('textbox', { name: 'Description' }).fill('Test');
+  await page.getByRole('spinbutton', { name: 'Available Slots' }).click();
+  await page.getByRole('spinbutton', { name: 'Available Slots' }).fill('10');
+  await page.getByRole('button', { name: 'Create Event' }).click();
+  await page.getByRole('button', { name: 'Log Out' }).click();
+  await page.getByRole('link', { name: 'Return To Public Site' }).click();
+  return newEventName;
+}
+
 module.exports = {
     loginAsAdmin,
-    loginAsUser
+    loginAsUser,
+    createTestEvent
 }
