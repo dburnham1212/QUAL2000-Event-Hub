489c59aa3074c6bdcfa3c64fe96ad0e07156fb43
Tania H <tania.horvat@student.sl.on.ca>
2026-04-20 00:13:33 -0400

    Adding more tests

--- tests/registrationPage.spec.js
+++ tests/registrationPage.spec.js
@@ -1,38 +1,139 @@
-import { test, expect } from '@playwright/test';
-import { getBaseURL } from './utils/helper';
+import { test, expect } from "@playwright/test";
 
 test.beforeEach(async ({ page }) => {
-  await page.goto(`${getBaseURL()}/register`); 
+    await page.goto(`/register`);
 });
 
 // ============ Events Page Testing ============
-test('Testing if manual navigation to register page displays the register page', async ({ page }) => {
-  await expect(page.getByText('Create your account')).toBeVisible();
+test("Testing if manual navigation to register page displays the register page", async ({
+    page,
+}) => {
+    await expect(page.getByText("Create your account")).toBeVisible();
 });
 
-test('Testing if account registration is successful', async ({ page }) => {
-  await page.getByRole('textbox', { name: 'Full Name' }).click();
-  await page.getByRole('textbox', { name: 'Full Name' }).fill('Dylan');
-  await page.getByRole('textbox', { name: 'Email Address' }).click();
-  await page.getByRole('textbox', { name: 'Email Address' }).fill(`dburnham${Date.now()}@example.com`);
-  await page.getByRole('textbox', { name: 'Password' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill('123');
-  await page.getByRole('button', { name: 'Create Account' }).click();
-  await expect(page.getByText('Your account has been created')).toBeVisible();
+test("Testing if account registration is successful", async ({ page }) => {
+    await page.getByRole("textbox", { name: "Full Name" }).click();
+    await page.getByRole("textbox", { name: "Full Name" }).fill("Dylan");
+    await page.getByRole("textbox", { name: "Email Address" }).click();
+    await page
+        .getByRole("textbox", { name: "Email Address" })
+        .fill(`dburnham${Date.now()}@example.com`);
+    await page.getByRole("textbox", { name: "Password" }).click();
+    await page.getByRole("textbox", { name: "Password" }).fill("123");
+    await page.getByRole("button", { name: "Create Account" }).click();
+    await expect(page.getByText("Your account has been created")).toBeVisible();
 });
 
-test('Testing if duplicate account registration is unsuccessful', async ({ page }) => {
-  await page.getByRole('textbox', { name: 'Full Name' }).click();
-  await page.getByRole('textbox', { name: 'Full Name' }).fill('Dylan');
-  await page.getByRole('textbox', { name: 'Email Address' }).click();
-  await page.getByRole('textbox', { name: 'Email Address' }).fill('dburnham@example.com');
-  await page.getByRole('textbox', { name: 'Password' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill('123');
-  await page.getByRole('button', { name: 'Create Account' }).click();
-  await expect(page.locator('section')).toContainText('An account already exists for that email address.');
+test("Testing if duplicate account registration is unsuccessful", async ({
+    page,
+}) => {
+    await page.getByRole("textbox", { name: "Full Name" }).click();
+    await page.getByRole("textbox", { name: "Full Name" }).fill("Dylan");
+    await page.getByRole("textbox", { name: "Email Address" }).click();
+    await page
+        .getByRole("textbox", { name: "Email Address" })
+        .fill("dburnham@example.com");
+    await page.getByRole("textbox", { name: "Password" }).click();
+    await page.getByRole("textbox", { name: "Password" }).fill("123");
+    await page.getByRole("button", { name: "Create Account" }).click();
+    await expect(page.locator("section")).toContainText(
+        "An account already exists for that email address.",
+    );
 });
 
-test('Testing if clicking on "Already Have An Account" button takes you to the login page', async ({ page }) => {
-  await page.getByRole('link', { name: 'Already Have An Account?' }).click();
-  await expect(page.getByRole('heading')).toContainText('Log in to manage your events.');
+test('Testing if clicking on "Already Have An Account" button takes you to the login page', async ({
+    page,
+}) => {
+    await page.getByRole("link", { name: "Already Have An Account?" }).click();
+    await expect(page.getByRole("heading")).toContainText(
+        "Log in to manage your events.",
+    );
+});
+
+test("Testing if registration works with empty name field", async ({
+    page,
+}) => {
+    await page.getByRole("textbox", { name: "Email Address" }).click();
+    await page.getByRole("button", { name: "Create Account" }).click();
+    const isValid = await page
+        .getByRole("textbox", { name: "Full Name" })
+        .evaluate((el) => el.checkValidity());
+    expect(isValid).toBe(false);
+});
+
+test("Testing if registration works with empty email field", async ({
+    page,
+}) => {
+    await page.getByRole("textbox", { name: "Full Name" }).fill("dylan");
+    await page.getByRole("button", { name: "Create Account" }).click();
+    const isValid = await page
+        .getByRole("textbox", { name: "Email Address" })
+        .evaluate((el) => el.checkValidity());
+    expect(isValid).toBe(false);
+});
+
+test("Testing if registration works with invalid email in field", async ({
+    page,
+}) => {
+    await page.getByRole("textbox", { name: "Full Name" }).fill("dylan");
+    await page.getByRole("textbox", { name: "Email Address" }).click();
+    await page.getByRole("textbox", { name: "Email Address" }).fill("dburnham");
+    await page.getByRole("button", { name: "Create Account" }).click();
+    const isValid = await page
+        .getByRole("textbox", { name: "Email Address" })
+        .evaluate((el) => el.checkValidity());
+    expect(isValid).toBe(false);
+});
+
+test("Testing if registration works with empty password field", async ({
+    page,
+}) => {
+    await page.getByRole("textbox", { name: "Full Name" }).fill("dylan");
+    await page.getByRole("textbox", { name: "Email Address" }).click();
+    await page
+        .getByRole("textbox", { name: "Email Address" })
+        .fill("dburnham@example.com");
+    await page.getByRole("button", { name: "Create Account" }).click();
+    const isValid = await page
+        .getByRole("textbox", { name: "Password" })
+        .evaluate((el) => el.checkValidity());
+    expect(isValid).toBe(false);
+});
+
+test.fixme("BUG: Testing if registration works with special characters", async ({
+    page,
+}) => {
+    await page.getByRole("textbox", { name: "Full Name" }).fill("%%");
+    await page.getByRole("textbox", { name: "Email Address" }).click();
+    await page
+        .getByRole("textbox", { name: "Email Address" })
+        .fill("%%@example.com");
+    await page.getByRole("textbox", { name: "Password" }).click();
+    await page.getByRole("textbox", { name: "Password" }).fill("123");
+    await page.getByRole("button", { name: "Create Account" }).click();
+    const isNameValid = await page
+        .getByRole("textbox", { name: "Full Name" })
+        .evaluate((el) => el.checkValidity());
+    expect(isNameValid).toBe(false);
+    const isEmailValid = await page
+        .getByRole("textbox", { name: "Email Address" })
+        .evaluate((el) => el.checkValidity());
+    expect(isEmailValid).toBe(false);
+});
+
+test("Testing if registration works with bad email (@@example.com)", async ({
+    page,
+}) => {
+    await page.getByRole("textbox", { name: "Full Name" }).fill("dylan");
+    await page.getByRole("textbox", { name: "Email Address" }).click();
+    await page
+        .getByRole("textbox", { name: "Email Address" })
+        .fill("@@example.com");
+    await page.getByRole("textbox", { name: "Password" }).click();
+    await page.getByRole("textbox", { name: "Password" }).fill("123");
+    await page.getByRole("button", { name: "Create Account" }).click();
+    const isEmailValid = await page
+        .getByRole("textbox", { name: "Email Address" })
+        .evaluate((el) => el.checkValidity());
+    expect(isEmailValid).toBe(false);
 });
--- tests/singleEventRegistrationForm.spec.js
+++ tests/singleEventRegistrationForm.spec.js
@@ -1,35 +1,61 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
+import { loginAsUser } from "./utils/helper";
 
-test('Testing if clicking "Add To My Calendar" button adds the event successfully', async ({ page }) => {
-  await page.goto('http://localhost:3000/login');
-  await page.getByRole('textbox', { name: 'Email Address' }).click();
-  await page.getByRole('textbox', { name: 'Email Address' }).fill('dburnham@example.com');
-  await page.getByRole('textbox', { name: 'Password' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill('123');
-  await page.getByRole('button', { name: 'Log In' }).click();
-  await page.getByRole('link', { name: 'Browse More Events' }).click();
-  await page.getByRole('link', { name: 'Music May 14, 2026 Kingston' }).click();
-  await page.getByRole('link', { name: 'Register For This Event' }).click();
-  await page.getByRole('button', { name: 'Add To My Calendar' }).click();
-  await expect(page.locator('body')).toContainText('Registration created successfully.');
+test.beforeEach(async ({ page }) => {
+    await loginAsUser(page);
+    await page.goto("/events");
 });
 
-test('Testing if clicking "Back To Event" button returns to the event successfully', async ({ page }) => {
-  await page.goto('http://localhost:3000/events/registrations');
-  await page.getByRole('link', { name: 'Browse More Events' }).click();
-  await page.getByRole('link', { name: 'Music May 14, 2026 Kingston' }).click();
-  await page.getByRole('link', { name: 'Register For This Event' }).click();
-  await page.getByRole('link', { name: 'Back To Event' }).click();
-  await expect(page.locator('body')).toContainText('Single event details');
+test('Testing if clicking "Add To My Calendar" button adds the event successfully', async ({
+    page,
+}) => {
+    await page
+        .getByRole("link", { name: "Music May 14, 2026 Kingston" })
+        .click();
+    await page.getByRole("link", { name: "Register For This Event" }).click();
+    await page.getByRole("button", { name: "Add To My Calendar" }).click();
+    await expect(page.locator("body")).toContainText(
+        "Registration created successfully.",
+    );
 });
 
-test('Testing if clicking "Update My Seats" and updating seat count updates the registration', async ({ page }) => {
-  await page.goto('http://localhost:3000/events/registrations');
-  await page.getByRole('link', { name: 'Browse More Events' }).click();
-  await page.getByRole('link', { name: 'Music May 14, 2026 Kingston' }).click();
-  await page.getByRole('link', { name: 'Update My Seats' }).click();
-  await page.getByRole('spinbutton', { name: 'Seat Count' }).click();
-  await page.getByRole('spinbutton', { name: 'Seat Count' }).fill('4');
-  await page.getByRole('button', { name: 'Save Seat Changes' }).click();
-  await expect(page.locator('body')).toContainText('Registration updated successfully.');
+test('Testing if clicking "Back To Event" button returns to the event successfully', async ({
+    page,
+}) => {
+    await page
+        .getByRole("link", { name: "Music May 14, 2026 Kingston" })
+        .click();
+    await page.getByRole("link", { name: "Register For This Event" }).click();
+    await page.getByRole("link", { name: "Back To Event" }).click();
+    await expect(page.locator("body")).toContainText("Single event details");
+});
+
+test('Testing if clicking "Update My Seats" and updating seat count updates the registration', async ({
+    page,
+}) => {
+    await page
+        .getByRole("link", { name: "Music May 14, 2026 Kingston" })
+        .click();
+    await page.getByRole("link", { name: "Update My Seats" }).click();
+    await page.getByRole("spinbutton", { name: "Seat Count" }).click();
+    await page.getByRole("spinbutton", { name: "Seat Count" }).fill("4");
+    await page.getByRole("button", { name: "Save Seat Changes" }).click();
+    await expect(page.locator("body")).toContainText(
+        "Registration updated successfully.",
+    );
+});
+
+test("Testing if you can update seats to a negative value", async ({
+    page,
+}) => {
+    await page
+        .getByRole("link", { name: "Music May 14, 2026 Kingston" })
+        .click();
+    await page.getByRole("link", { name: "Register For This Event" }).click();
+    await page.getByRole("button", { name: "Add To My Calendar" }).click();
+    await page.getByRole("link", { name: "Edit Seats" }).click();
+    await page.getByRole("spinbutton", { name: "Seat Count" }).click();
+    await page.getByRole("spinbutton", { name: "Seat Count" }).fill("-1");
+    await page.getByRole("spinbutton", { name: "Seat Count" }).press("Enter");
+    await page.getByRole("button", { name: "Save Seat Changes" }).click();
 });
